05d4d89cbd500d6e5c2745643771f8c35b742cd9
Yana Harcourt <yana.harcourt@gmail.com>
2026-03-08 15:09:07 +0100

    commit

---
+++ supabase-init.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://kfoweeagmvzommlrxxjm.supabase.co',
+  'sb_publishable_Chq-fxxL7NbVgkWarl7gMQ_xABwrDpD'
+)
